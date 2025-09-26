2612e6c3dac1f395e2e624395ab7bf0e111fed2e
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-09-25 19:16:26 -0700

    can create root with array

--- BinaryTree.js
+++ BinaryTree.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(data, leftNode, rightNode) {
+  constructor(data, leftNode = null, rightNode = null) {
     this.data = data;
     this.leftNode = leftNode;
     this.rightNode = rightNode;
@@ -7,8 +7,20 @@ class Node {
 }
 
 class Tree {
-  constructor(arr) {
-    this.arr = arr;
+  constructor(array) {
+    this.array = array;
     this.root = null;
   }
+
+  buildTree() {
+    const arr = this.array;
+    if (this.root == null) {
+      const rootData = arr[(arr.length - 1) / 2];
+      this.root = new Node(rootData);
+    }
+  }
 }
+
+const tree = new Tree([1, 3, 5, 7, 10, 11, 15]);
+
+tree.buildTree();
