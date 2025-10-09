f201d785996ad32ef7b63eeab70f4ee79ad75f30
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-10-09 01:01:16 -0700

    deleting items in tree now seems to work

--- BinaryTree.js
+++ BinaryTree.js
@@ -16,7 +16,6 @@ class Tree {
     const noDuplicates = new Set(this.array);
     const newArray = [...noDuplicates];
     const arr = newArray.sort((a, b) => a - b);
-    console.log("Array: ", arr);
     this.root = this.recursiveTree(arr, 0, arr.length - 1);
   }
 
@@ -40,13 +39,10 @@ class Tree {
     // Find where to put leaf node on tree
     while (current != null) {
       parent = current;
-      console.log("Value to insert:", value, "/ current.data: ", current.data);
       if (value < current.data) {
         current = current.left;
-        console.log("go left");
       } else if (value > current.data) {
         current = current.right;
-        console.log("go right");
       }
     }
 
@@ -55,15 +51,69 @@ class Tree {
     } else {
       parent.right = newValue;
     }
-    // this.current = newValue;
     return newValue;
   }
-  // delete(value) {}
+
+  deleteItem(value) {
+    let current = this.root;
+    let parent = null;
+    let traveledLeftSide = false;
+    let childData = [];
+
+    // Recursive function to retrieve data from children
+    const retrieveChildData = (child) => {
+      if (child == null) return;
+
+      childData.push(child.data);
+
+      retrieveChildData(child.left);
+      retrieveChildData(child.right);
+    };
+
+    // In case the value is the root value of binary tree
+    if (value == this.root.data) {
+      retrieveChildData(this.root.left);
+      retrieveChildData(this.root.right);
+
+      this.array = childData;
+      this.buildTree();
+      return this.root.data;
+    }
+
+    // Traverse tree to find parent node of node to be deleted
+    while (current.data != value) {
+      parent = current;
+      if (value < current.data) {
+        current = current.left;
+        traveledLeftSide = true;
+      } else if (value > current.data) {
+        current = current.right;
+        traveledLeftSide = false;
+      }
+    }
+
+    // Place data of deleted item's children into childData array
+    retrieveChildData(current.left);
+    retrieveChildData(current.right);
+
+    if (traveledLeftSide == true && childData.length == 0) {
+      parent.left = null;
+    } else if (traveledLeftSide == false && childData.length == 0) {
+      parent.right = null;
+    } else if (traveledLeftSide == true && childData.length > 0) {
+      parent.left = null;
+      childData.map((item) => {
+        this.insert(item);
+      });
+    } else if (traveledLeftSide == false && childData.length > 0) {
+      parent.right = null;
+      childData.map((item) => {
+        this.insert(item);
+      });
+    }
+    return current.data;
+  }
 }
-
-const tree = new Tree([1, 3, 5, 7, 9, 15, 21]);
-
-tree.buildTree();
 
 // Function to visualize tree:
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -79,10 +129,19 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
+const tree = new Tree([1, 3, 5, 7, 9, 15, 21]);
+
+tree.buildTree();
+
+console.log(tree.array);
+
 prettyPrint(tree.root);
 
-tree.insert(2);
+console.log("New tree after deletion(s): ");
 
-console.log("New tree after insertion: ");
+tree.deleteItem(3);
+tree.deleteItem(15);
+tree.deleteItem(21);
+tree.deleteItem(7);
 
 prettyPrint(tree.root);
